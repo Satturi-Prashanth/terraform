dd4a3ce66bb67b87fa8aa998c1daad8357353111
sathuri <prashanthsatturi913@gmail.com>
2025-07-10 17:07:23 +0530

    loadbalancer commit

--- ec2.tf
+++ ec2.tf
@@ -30,3 +30,41 @@ resource "aws_instance" "Data" {
     }
   
 }
+
+# Create a LoadBalancer 
+
+resource "aws_lb" "example" {
+  name = "example-load-balancer"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.example.id]
+  subnets = [aws_subnet.subnet_1.id,aws_subnet.subnet_2.id]
+  tags = {
+    Name = "Clahan-LB"
+  }
+  
+}
+
+# Create a Target Group 
+
+resource "aws_lb_target_group" "example" {
+  name = "example-taget-group"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.clahan_vpc.id
+  tags = {
+    Name = "Clahan-Target"
+  }
+}
+
+# Create a Listener 
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+}
